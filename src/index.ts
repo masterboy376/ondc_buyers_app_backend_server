912c5564cb0a770f3e8c779de58ab89b3dfe469c
sambhav kaushik <masterboy376@gmail.com>
2023-05-15 13:10:29 +0530

    organised the routes in different files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+// imports
 import express, { Express, Request, Response } from 'express';
-import { createUser, changePassword } from './routes/user.js';
 
+// routes
+import userRoutes from './routes/user.js';
+import cartRoutes from './routes/cart.js';
+import orderRoutes from './routes/order.js';
+
+// important variables
 const app:Express = express();
 const port:number = 3000;
 
-app.use('/user', [ createUser, changePassword ]);
+// endpoints
+app.use('/user', userRoutes);
+app.use('/cart', cartRoutes);
+app.use('/order', orderRoutes);
 
+// server init 
 app.listen(port, ()=>{
-    console.log(`the app is running at port ${port}`);
+    console.log(`The app is running at url http://localhost:${port}/`);
 });
--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,10 +2,17 @@ import express, { Express, Request, Response } from 'express';
 
 const router = express.Router();
 
-export const createUser = router.get('/createUser', (req:Request, res:Response)=>{
+// routes
+//---start---
+
+router.get('/createUser', (req:Request, res:Response)=>{
     res.status(200).send("all is well");
 });
 
-export const changePassword = router.get('/changePassword', (req:Request, res:Response)=>{
-    res.status(200).send("all is well");
+router.get('/changePassword', (req:Request, res:Response)=>{
+    res.status(200).send("all is well, sort of");
 });
+
+//---end---
+
+export default router;
